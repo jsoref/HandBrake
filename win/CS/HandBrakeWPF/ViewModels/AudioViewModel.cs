3eb60987ae2228edd05694f01c281b536eb8d7fc
Josh Soref <jsoref@users.noreply.github.com>
2020-08-26 11:16:27 -0400

    spelling: contains
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- win/CS/HandBrakeWPF/ViewModels/AudioViewModel.cs
+++ win/CS/HandBrakeWPF/ViewModels/AudioViewModel.cs
@@ -566,7 +566,7 @@ namespace HandBrakeWPF.ViewModels
             // For all the source audio tracks
             foreach (Audio sourceTrack in this.SourceTracks)
             {
-                // Step 2: Check if the track list already contrains this track
+                // Step 2: Check if the track list already contains this track
                 bool found = this.Task.AudioTracks.Any(audioTrack => Equals(audioTrack.ScannedTrack, sourceTrack));
                 if (!found)
                 {
@@ -584,7 +584,7 @@ namespace HandBrakeWPF.ViewModels
             // Add them if they are not already added.
             foreach (Audio sourceTrack in this.GetSelectedLanguagesTracks())
             {
-                // Step 2: Check if the track list already contrains this track
+                // Step 2: Check if the track list already contains this track
                 bool found = this.Task.AudioTracks.Any(audioTrack => Equals(audioTrack.ScannedTrack, sourceTrack));
                 if (!found)
                 {
